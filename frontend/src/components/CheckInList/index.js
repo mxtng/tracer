d57f7b1fcc1698cdae60cce730b9711ec40539e9
Melvin Tang <mxcodeyard@gmail.com>
2020-11-27 20:21:19 +0800

    Fix check-in table list

--- frontend/src/components/CheckInList/index.js
+++ frontend/src/components/CheckInList/index.js
@@ -7,7 +7,7 @@ const CheckInList = ({ list }) => {
       <Table striped bordered hover size='sm'>
         <thead>
           <tr>
-            <th>#</th>
+            <th>Check-In At</th>
             <th>Full Name</th>
             <th>NRIC</th>
             <th>Temperature</th>
@@ -22,17 +22,14 @@ const CheckInList = ({ list }) => {
             </tr>
           ) : (
             <Fragment>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan='2'>Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
+              {list.map(({ _id, fullName, nric, temperature, createdAt }) => (
+                <tr key={_id}>
+                  <td>{createdAt}</td>
+                  <td>{fullName}</td>
+                  <td>{nric}</td>
+                  <td>{temperature}</td>
+                </tr>
+              ))}
             </Fragment>
           )}
         </tbody>
