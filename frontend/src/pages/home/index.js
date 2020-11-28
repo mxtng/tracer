8ce90c4ff2aa66f1c3e6b13319904b658151d07f
Melvin Tang <mxcodeyard@gmail.com>
2020-11-28 09:18:36 +0800

    Create homepage

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import withNav from '../../utils/withNav';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Button, Card, Row, Col } from 'react-bootstrap';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Jumbotron className='mt-4'>
+        <h1>Contact Tracing for Businesses</h1>
+        <p>
+          A customized solution for businesses that want to manage their own
+          contact tracing in the event of this COVID-19 pandemic
+        </p>
+        <Link to='/register'>
+          <Button>Register Now</Button>
+        </Link>
+      </Jumbotron>
+      <Row>
+        <Col className='mb-2 col-sm-12 col-md-4'>
+          <Card bg='primary' style={{ height: '12rem' }}>
+            <Card.Title className='text-center mt-2 mx-3 border-bottom'>
+              <h4>Step One</h4>
+            </Card.Title>
+            <Card.Body style={{ color: '#515150' }}>Register with us</Card.Body>
+          </Card>
+        </Col>
+        <Col className='mb-2 col-sm-12 col-md-4'>
+          <Card bg='primary' style={{ height: '12rem' }}>
+            <Card.Title className='text-center mt-2 mx-3 border-bottom'>
+              <h4>Step Two</h4>
+            </Card.Title>
+            <Card.Body style={{ color: '#515150' }}>
+              Sign in & generate a customized check-in link
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className='md-2 col-sm-12 col-md-4'>
+          <Card bg='primary' style={{ height: '12rem' }}>
+            <Card.Title className='text-center mt-2 mx-3 border-bottom'>
+              <h4>Step Three</h4>
+            </Card.Title>
+            <Card.Body style={{ color: '#515150' }}>
+              Customers check-in with generated link before entering the
+              business premises
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default withNav(Home);
